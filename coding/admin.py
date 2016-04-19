4c76e6fd2468fd000082ec3e7733bfd4cad81c4b
deepthy123 <deepthyjosev@gmail.com>
2016-04-19 15:13:42 +0530

    new input

--- coding/admin.py
+++ coding/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.from django.contrib import admin
+
+from .models import Question, Contest, User, Test_case
+
+admin.site.register(Question)
+admin.site.register(Contest)
+admin.site.register(User)
+admin.site.register(Test_case)
--- coding/models.py
+++ coding/models.py
@@ -3,3 +3,30 @@
 # Create your models here.
 from django.db import models
 
+
+class Contest(models.Model):
+	contest_name = models.CharField(max_length=200)
+	college_name = models.CharField(max_length=200)
+	no_of_candidates = models.IntegerField(default=0)
+	no_of_questions = models.IntegerField(default=0)
+	def __str__(self):
+		return self.contest_name 
+
+class User(models.Model):
+	contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+	user_name = models.CharField(max_length=200)
+	def __str__(self):
+		return self.user_name 
+
+class Question(models.Model):
+	contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+	question_name = models.CharField(max_length=200)
+	def __str__(self):
+		return self.question_name 
+
+class Test_case (models.Model):
+	#contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	cases = models.CharField(max_length=200)
+	def __str__(self):
+		return self.cases 
